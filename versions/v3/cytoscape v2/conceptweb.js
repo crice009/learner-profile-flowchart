755f0a65c62fcfb0129bcd050cecf2b7268831b5
Corey Rice <crice009@gmail.com>
2020-07-04 16:17:45 -0400

    added concentirc layout to mock-up graph, and some configuration options

--- versions/v3/cytoscape v2/conceptweb.js
+++ versions/v3/cytoscape v2/conceptweb.js
@@ -1,38 +1,43 @@
 var cy = cytoscape({
     container: document.getElementById('cy'),
-    elements: [{
-        data: { id: 'a' }
-    }, {
-        data: { id: 'b' }
-    }, {
-        data: { id: 'c' }
-    }, {
-        data: { id: 'd' }
-    }, {
-        data: { id: 'e' }
-    }, {
-        data: { id: 'ab', source: 'a', target: 'b' }
-    }, {
-        data: { id: 'ac', source: 'a', target: 'c' }
-    }, {
-        data: { id: 'ae', source: 'a', target: 'e' }
-    }, {
-        data: { id: 'bc', source: 'b', target: 'c' }
-    }, {
-        data: { id: 'bd', source: 'b', target: 'd' }
-    }, {
-        data: { id: 'ce', source: 'c', target: 'e' }
-    }],
+    elements: {
+        nodes: [
+            {data: { id: 'n1', name:'Fab Lab Skills' }}, 
+            {data: { id: 'n2', name:'3D Design' }}, 
+            {data: { id: 'n3', name:'2D Design' }}, 
+            {data: { id: 'n4', name:'Fab Lab Mindset' }},
+            {data: { id: 'n5', name:'Computational Thinking' }}, 
+            {data: { id: 'n6', name:'Electronics' }}, 
+            {data: { id: 'n7', name:'Microcontrollers' }}
+        ],
+        edges: [
+
+            {data: { id: '1-2', source: 'n1', target: 'n2' }}, 
+            {data: { id: '1-3', source: 'n1', target: 'n3' }}, 
+            {data: { id: '1-4', source: 'n1', target: 'n4' }}, 
+            {data: { id: '1-5', source: 'n1', target: 'n5' }}, 
+            {data: { id: '1-6', source: 'n1', target: 'n6' }}, 
+            {data: { id: '1-7', source: 'n1', target: 'n7' }}
+        ]},
     style: [{ //http://manual.cytoscape.org/en/stable/Styles.html#introduction-to-style
         selector: 'node',
         style: {
-            shape: 'hexagon',
+            shape: 'concentric',
             'background-color': 'green',
-            label: 'data(id)'
+            'color': 'white',
+            label: 'data(name)'
         }
     }],
     layout: {
-        name: 'circle'
-    }
+        name: 'concentric', //https://js.cytoscape.org/#layouts/concentric
+        spacingFactor: 5,
+        concentric: function( node ){
+          return node.degree();
+        },
+        levelWidth: function( nodes ){
+          return 1;
+        }
+      },
+
 
 });
