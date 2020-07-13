a7897bf2572f8a82cb046d0ddba03fe4dbbf8cf7
Corey Rice <crice009@gmail.com>
2020-07-12 21:08:35 -0400

    Now the JSON example really parses two JSON strings and turns them into JS arrays that can be used by cytoscape. The sequence file was changed accordingly to make this parsed JSON work fine (since the sequence numbers had a change in format on JSON.parse()

--- versions/v4/json/conceptweb.js
+++ versions/v4/json/conceptweb.js
@@ -1,25 +1,51 @@
+//var JSON_nodes_and_edges = '{"elements": [{"nodes": [{ "data": [{ "id": "n1", "order": "1", "name": "Fab Lab Skills", "href": "https://prezi.com/view/zBqbXsXRkA4yfSB1OiAT/" }] },{ "data": [{ "id": "n2", "order": "4", "name": "3D Design" }] },{ "data": [{ "id": "n3", "order": "3", "name": "2D Design" }] },{ "data": [{ "id": "n4", "order": "2", "name": "Fab Lab Mindset" }] },{ "data": [{ "id": "n5", "order": "5", "name": "Computational Thinking" }] },{ "data": [{ "id": "n6", "order": "6", "name": "Electronics" }] },{ "data": [{ "id": "n7", "order": "7", "name": "Microcontrollers" }] }],"edges": [{ "data": [{ "id": "1-2", "source": "n1", "target": "n2" }] },{ "data": [{ "id": "1-3", "source": "n1", "target": "n3" }] },{ "data": [{ "id": "1-4", "source": "n1", "target": "n4" }] },{ "data": [{ "id": "1-5", "source": "n1", "target": "n5" }] },{ "data": [{ "id": "1-6", "source": "n1", "target": "n6" }] },{ "data": [{ "id": "1-7", "source": "n1", "target": "n7" }] }]}]}';
+
+var JSON_nodes = '[{"data": { "id": "n1", "order": "1", "name": "Fab Lab Skills", "href": "https://prezi.com/view/zBqbXsXRkA4yfSB1OiAT/" } },{ "data": { "id": "n2", "order": "4", "name": "3D Design" } },{ "data": { "id": "n3", "order": "3", "name": "2D Design" } },{ "data": { "id": "n4", "order": "2", "name": "Fab Lab Mindset" } },{ "data": { "id": "n5", "order": "5", "name": "Computational Thinking" } },{ "data": { "id": "n6", "order": "6", "name": "Electronics" } },{ "data": { "id": "n7", "order": "7", "name": "Microcontrollers" } }]';
+var JSON_edges = '[{"data": { "id": "1-2", "source": "n1", "target": "n2" } },                                                               { "data": { "id": "1-3", "source": "n1", "target": "n3" } },  { "data": { "id": "1-4", "source": "n1", "target": "n4" } },  { "data": { "id": "1-5", "source": "n1", "target": "n5" } },        { "data": { "id": "1-6", "source": "n1", "target": "n6" } },               { "data": { "id": "1-7", "source": "n1", "target": "n7" } }]';
+
+//var data = JSON.parse(text);
+var nodesData = JSON.parse(JSON_nodes);
+// console.log(nodesData.length); //thought I needed this when I still didn't understand the JSON/ JS parsing dynamic. 
+// for (var i = 0; i < nodesData.length; i++) {  //keeping it around in case things change when a DB is implemented...
+//     var _nodes = nodesData[i];
+// }
+
+console.log(nodesData);
+
+var edgesData = JSON.parse(JSON_edges);
+// console.log(edgesData);  //thought I needed this when I still didn't understand the JSON/ JS parsing dynamic. 
+// for (var i = 0; i < edgesData.length; i++) { //keeping it around in case things change when a DB is implemented...
+//     var _edges = edgesData[i];
+// }
+
 var cy = cytoscape({
     container: document.getElementById('cy'),
-    elements: {
-        nodes: [
-            { data: { id: 'n1', order: 1, name: 'Fab Lab Skills', href: 'https://prezi.com/view/zBqbXsXRkA4yfSB1OiAT/' } },
-            { data: { id: 'n2', order: 4, name: '3D Design' } },
-            { data: { id: 'n3', order: 3, name: '2D Design' } },
-            { data: { id: 'n4', order: 2, name: 'Fab Lab Mindset' } },
-            { data: { id: 'n5', order: 5, name: 'Computational Thinking' } },
-            { data: { id: 'n6', order: 6, name: 'Electronics' } },
-            { data: { id: 'n7', order: 7, name: 'Microcontrollers' } }
-        ],
-        edges: [
 
-            { data: { id: '1-2', source: 'n1', target: 'n2' } },
-            { data: { id: '1-3', source: 'n1', target: 'n3' } },
-            { data: { id: '1-4', source: 'n1', target: 'n4' } },
-            { data: { id: '1-5', source: 'n1', target: 'n5' } },
-            { data: { id: '1-6', source: 'n1', target: 'n6' } },
-            { data: { id: '1-7', source: 'n1', target: 'n7' } }
-        ]
+    elements: {
+        nodes: nodesData,
+        edges: edgesData
     },
+
+    // elements: {
+    //     nodes: [
+    //         { data: { id: 'n1', order: 1, name: 'Fab Lab Skills', href: 'https://prezi.com/view/zBqbXsXRkA4yfSB1OiAT/' } },
+    //         { data: { id: 'n2', order: 4, name: '3D Design' } },
+    //         { data: { id: 'n3', order: 3, name: '2D Design' } },
+    //         { data: { id: 'n4', order: 2, name: 'Fab Lab Mindset' } },
+    //         { data: { id: 'n5', order: 5, name: 'Computational Thinking' } },
+    //         { data: { id: 'n6', order: 6, name: 'Electronics' } },
+    //         { data: { id: 'n7', order: 7, name: 'Microcontrollers' } }
+    //     ],
+    //     edges: [
+    //         { data: { id: '1-2', source: 'n1', target: 'n2' } },
+    //         { data: { id: '1-3', source: 'n1', target: 'n3' } },
+    //         { data: { id: '1-4', source: 'n1', target: 'n4' } },
+    //         { data: { id: '1-5', source: 'n1', target: 'n5' } },
+    //         { data: { id: '1-6', source: 'n1', target: 'n6' } },
+    //         { data: { id: '1-7', source: 'n1', target: 'n7' } }
+    //     ]
+    // },
+
     style: [{ //http://manual.cytoscape.org/en/stable/Styles.html#introduction-to-style
         selector: 'node',
         style: {
--- versions/v4/json/sequence.js
+++ versions/v4/json/sequence.js
@@ -10,20 +10,20 @@ var sequence_position = 0;
 
 function advance() {
     sequence_position += 1;
-    if (sequence_position > 7) sequence_position = 0;
-
-    var node = cy.filter('node[order = ' + String(sequence_position) + ']');
+    if (sequence_position > nodesData.length) sequence_position = 1; //right now the 'order' is 1-referenced, but that should change to 0-referenced in future versions
+    console.log(sequence_position);
+    var node = cy.filter('node[ order = "' + String(sequence_position) + '"]');
     console.log(node);
-    cy.fit(node, 400); //150 is the relative pixel count around the selected item...
+    cy.fit(node, 150); //150 is the relative pixel count around the selected item...
     document.getElementById('sequence').classList.add("on-page");
     document.getElementById('map').classList.remove("on-page");
 };
 
 function regress() {
     sequence_position -= 1;
-    if (sequence_position < 0) sequence_position = 7;
-
-    var node = cy.filter('node[order = ' + String(sequence_position) + ']');
+    if (sequence_position < 1) sequence_position = 7; //right now the 'order' is 1-referenced, but that should change to 0-referenced in future versions
+    console.log(sequence_position);
+    var node = cy.filter('node[ order = "' + String(sequence_position) + '"]');
     console.log(node);
     cy.fit(node, 150); //150 is the relative pixel count around the selected item...
     document.getElementById('sequence').classList.add("on-page");
