9e9e8f299923fff84a0edd2ce35b6f987a406296
Corey Rice <crice009@gmail.com>
2024-01-31 21:18:40 -0500

    basics for adding a 'MapVue' page

---
+++ vue-app/learner-profile-flowchart/src/views/MapView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="map">
+    <h1>This is a map page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<!-- <script lang="ts">
+  import cytoscape from "@/libs/cytoscape.min.3.28.1.js";
+</script> -->
