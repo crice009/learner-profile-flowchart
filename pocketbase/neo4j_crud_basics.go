9cda36c75a32d52a088b098271983c823588c74b
Corey Rice <crice009@gmail.com>
2024-02-02 21:16:07 -0500

    CRUD edges

--- pocketbase/neo4j_crud_basics.go
+++ pocketbase/neo4j_crud_basics.go
@@ -36,3 +36,23 @@ func (db *GraphDB) DeleteNode(id int64) error {
 	// Implement node deletion here
 	return nil
 }
+
+func (db *GraphDB) CreateRelationship(startNodeID, endNodeID int64, relationshipType string, properties map[string]interface{}) (*neo4j.Relationship, error) {
+	// Implement relationship creation here
+	return nil, nil
+}
+
+func (db *GraphDB) ReadRelationship(id int64) (*neo4j.Relationship, error) {
+	// Implement relationship reading here
+	return nil, nil
+}
+
+func (db *GraphDB) UpdateRelationship(id int64, properties map[string]interface{}) (*neo4j.Relationship, error) {
+	// Implement relationship updating here
+	return nil, nil
+}
+
+func (db *GraphDB) DeleteRelationship(id int64) error {
+	// Implement relationship deletion here
+	return nil
+}
