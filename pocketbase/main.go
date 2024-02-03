fef1207681874734c852a47169ae7104636377b9
Corey Rice <crice009@gmail.com>
2024-02-02 22:27:39 -0500

    fix(pb_serve): make sure neo4j and pb_serve are using the same username/password for the database

--- pocketbase/main.go
+++ pocketbase/main.go
@@ -12,16 +12,8 @@ import (
 func main() {
 	app := pocketbase.New()
 
-	graph_db_username := os.Getenv("GRAPH_DB_USERNAME")
-	graph_db_password := os.Getenv("GRAPH_DB_PASSWORD")
-
-	db, err := NewGraphDB("bolt://localhost:7687", graph_db_username, graph_db_password)
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Printf("Connected to Neo4j database, without error.")
-		log.Printf("Database: %v", db)
-	}
+	graph_db_username := os.Getenv("NEO4J_USERNAME")
+	graph_db_password := os.Getenv("NEO4J_PASSWORD")
 
 	// serves static files from the provided public dir (if exists)
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
